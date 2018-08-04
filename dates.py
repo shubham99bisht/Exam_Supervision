15f40571704eee20bf1b990dea6277f8e52075d6
shubham <shubham99bisht@gmail.com>
2018-08-04 23:37:46 +0530

    Delete dates.py

--- dates.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-def write_dates_csv(lis):
-    with open('date.csv','a',newline='') as csvfile:
-      date_writer = csv.writer(csvfile, delimiter=',')
-      date_writer.writerow(lis)
